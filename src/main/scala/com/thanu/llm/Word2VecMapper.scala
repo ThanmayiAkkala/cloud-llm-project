807a04bc61184826a9cb0f6b4f4f7cc2d9fc0bd3
Thanmayi Akkala <takkal2@uic.edu>
2024-10-05 22:14:19 -0500

    Changes

--- src/main/scala/com/thanu/llm/Word2VecMapper.scala
+++ src/main/scala/com/thanu/llm/Word2VecMapper.scala
@@ -1,58 +1,3 @@
-//package com.thanu.llm
-//
-//import org.apache.hadoop.io.{LongWritable, Text}
-//import org.apache.hadoop.mapreduce.Mapper
-//import org.deeplearning4j.models.word2vec.Word2Vec
-//import org.deeplearning4j.text.sentenceiterator.CollectionSentenceIterator
-//import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory
-//import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer
-//import scala.collection.mutable.ArrayBuffer
-//import scala.jdk.CollectionConverters._  // Import to use asJava
-//
-//class Word2VecMapper extends Mapper[LongWritable, Text, Text, Text] {
-//
-//  val sentencesBuffer: ArrayBuffer[String] = ArrayBuffer[String]()
-//
-//  override def map(key: LongWritable, value: Text, context: Mapper[LongWritable, Text, Text, Text]#Context): Unit = {
-//    // Collect tokens for training
-//    val sentence = value.toString
-//    sentencesBuffer += sentence
-//  }
-//
-//  override def cleanup(context: Mapper[LongWritable, Text, Text, Text]#Context): Unit = {
-//    // Once all tokens are gathered, create the Word2Vec model
-//    val tokenizerFactory = new DefaultTokenizerFactory()
-//    val sentenceIterator = new CollectionSentenceIterator(sentencesBuffer.asJava) // Convert to Java collection
-//
-//    // Build the Word2Vec model
-//    val word2Vec = new Word2Vec.Builder()
-//      .minWordFrequency(1)
-//      .iterations(10)
-//      .layerSize(10)
-//      .seed(42)
-//      .windowSize(5)
-//      .iterate(sentenceIterator)
-//      .tokenizerFactory(tokenizerFactory)
-//      .build()
-//
-//    // Train the model
-//    word2Vec.fit()
-//
-//    // Save the model for later use
-//    WordVectorSerializer.writeWord2VecModel(word2Vec, new java.io.File("word2vec_model.bin"))
-//
-//    // Output the embeddings for each token
-//    sentencesBuffer.foreach { sentence =>
-//      sentence.split("\\s+").foreach { token =>
-//        if (word2Vec.hasWord(token)) {
-//          val embedding = word2Vec.getWordVector(token)
-//          val embeddingString = embedding.mkString(",")
-//          context.write(new Text(token), new Text(embeddingString))
-//        }
-//      }
-//    }
-//  }
-//}
 package com.thanu.llm
 
 import org.apache.hadoop.io.{LongWritable, Text}
@@ -62,14 +7,16 @@ import org.deeplearning4j.text.sentenceiterator.CollectionSentenceIterator
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer
 import org.slf4j.{Logger, LoggerFactory}
-import scala.collection.mutable.ArrayBuffer
-import scala.jdk.CollectionConverters._  // Import to use asJava
+import scala.collection.mutable.{ArrayBuffer, HashSet}
+import scala.jdk.CollectionConverters._
 
 class Word2VecMapper extends Mapper[LongWritable, Text, Text, Text] {
 
   // Initialize logger
   val logger: Logger = LoggerFactory.getLogger(classOf[Word2VecMapper])
 
+  // Use HashSet to filter out duplicate tokens
+  val processedTokens: HashSet[String] = HashSet()
   val sentencesBuffer: ArrayBuffer[String] = ArrayBuffer[String]()
 
   override def setup(context: Mapper[LongWritable, Text, Text, Text]#Context): Unit = {
@@ -108,16 +55,24 @@ class Word2VecMapper extends Mapper[LongWritable, Text, Text, Text] {
     WordVectorSerializer.writeWord2VecModel(word2Vec, new java.io.File("word2vec_model.bin"))
     logger.info("Word2Vec model saved as word2vec_model.bin")
 
-    // Output the embeddings for each token
+    // Output the embeddings for each token (prevent duplicates)
     sentencesBuffer.foreach { sentence =>
       sentence.split("\\s+").foreach { token =>
-        if (word2Vec.hasWord(token)) {
+        if (word2Vec.hasWord(token) && !processedTokens.contains(token)) {
+          processedTokens += token
           val embedding = word2Vec.getWordVector(token)
           val embeddingString = embedding.mkString(",")
+
+          // Write the token and its embedding
           context.write(new Text(token), new Text(embeddingString))
-          logger.debug(s"Token: $token, Embedding: $embeddingString")
-          val similarTokens = word2Vec.wordsNearest(token, 5)
-          context.write(new Text(s"Tokens similar to '$token':"), new Text(similarTokens.mkString(", ")))
+          logger.debug(s"Token:' $token', Embedding: '$embeddingString'")
+
+          // Find and emit the tokens similar to the current token using cosine similarity
+          val similarTokens = word2Vec.wordsNearest(token, 5).asScala
+          logger.info(s"Tokens similar to '$token': ${similarTokens.mkString(", ")}")
+
+          // Emit the token and its similar tokens
+          context.write(new Text(s"Similar tokens to '$token':"), new Text(similarTokens.mkString(", ")))
         }
       }
     }
